9a399c63437e49bee2f0c179702cac32078b1e85
zhaohui <ksfzhaohui@126.com>
2021-11-15 21:54:43 +0800

    同时

--- src/main/java/org/simulationlogin/SimulationLogin.java
+++ src/main/java/org/simulationlogin/SimulationLogin.java
@@ -3,6 +3,7 @@ package org.simulationlogin;
 import org.simulationlogin.login.AbstractLogin;
 import org.simulationlogin.login.impl.HuaweiLogin;
 
+//dfdf
 public class SimulationLogin {
 
 	public static void main(String[] args) {
